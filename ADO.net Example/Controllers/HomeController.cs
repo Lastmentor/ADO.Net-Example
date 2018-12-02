94a16504cb1e0efed07f98caccbe78c89713ab58
Rıza Özonuk <riza.gs1905@hotmail.com>
2018-12-02 17:29:24 +0300

    Files Added

---
+++ ADO.net Example/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using ADO.net_Example.Models;
+
+namespace ADO.net_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            Student student = new Student();
+            DataTable dt = student.GetAllStudents();
+            return View(dt);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public ActionResult Add(FormCollection frm)
+        {
+            Student student = new Student();
+            string name = frm["S_Name"];
+            int age = Convert.ToInt32(frm["S_Age"]);
+            string gender = frm["S_Gender"];
+            int status = student.InsertStudent(name, gender, age);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int StudentID)
+        {
+            Student student = new Student();
+            student.DeleteStudent(StudentID);
+            return RedirectToAction("Index");
+        }        
+
+        public ActionResult Edit(int StudentID)
+        {
+            Student student = new Student();
+            DataTable dt = student.GetStudentByID(StudentID);
+            return View("Edit", dt);
+        }
+
+        public ActionResult UpdateRecord(FormCollection frm)
+        {
+            Student student = new Student();
+            string name = frm["S_Name"];
+            int age = Convert.ToInt32(frm["S_Age"]);
+            string gender = frm["S_Gender"];
+            int id = Convert.ToInt32(frm["S_ID"]);
+            int status = student.UpdateStudent(id, name, gender, age);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ADO.net Example/Models/Student.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.net_Example.Models
+{
+    public class Student
+    {
+        string connectionString = @"Data Source=LAPTOP-CILN0DIS;Initial Catalog=Test;Integrated Security=True";        
+
+        public DataTable GetAllStudents()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Student", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public int InsertStudent(string StudentName, string Gender, int Age)
+        {            
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Insert into Student (Student_Name, Student_Age, Student_Gender) values(@studname, @studage , @gender)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@studname", StudentName);
+                cmd.Parameters.AddWithValue("@studage", Age);
+                cmd.Parameters.AddWithValue("@gender", Gender);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int DeleteStudent(int StudentID)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();                
+                SqlCommand cmd = new SqlCommand("Delete from Student where Student_ID=@studid", con);
+                cmd.Parameters.AddWithValue("@studid", StudentID);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable GetStudentByID(int StudentID)
+        {
+            DataTable dt = new DataTable();            
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Student where Student_ID=@studid", con);
+                cmd.Parameters.AddWithValue("@studid", StudentID);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public int UpdateStudent(int StudentID, string StudentName, string Gender, int Age)
+        {            
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Update Student SET Student_Name=@studname, Student_Age=@studage , Student_Gender=@gender where Student_ID=@studid";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@studname", StudentName);
+                cmd.Parameters.AddWithValue("@studage", Age);
+                cmd.Parameters.AddWithValue("@gender", Gender);
+                cmd.Parameters.AddWithValue("@studid", StudentID);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
